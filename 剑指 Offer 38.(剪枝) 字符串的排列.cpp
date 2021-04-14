2f1eed60bbab648a957cb9f35adda0f7e72db5d5
GE Junyan <51085850+GeJunyan@users.noreply.github.com>
2021-04-14 22:33:38 +0800

    Update 剑指 Offer 38.(剪枝) 字符串的排列.cpp

--- 剑指 Offer 38.(剪枝) 字符串的排列.cpp
+++ 剑指 Offer 38.(剪枝) 字符串的排列.cpp
@@ -1,2 +1,36 @@
 /*输入一个字符串，打印出该字符串中字符的所有排列。*/
 
+class Solution {
+public:
+
+    //防止重复
+    set<string> store;
+    vector<string> res;
+
+    void dfs(string& s, string &cur, vector<bool> used)
+    {
+        if(cur.size() == s.size())
+        {
+            store.insert(cur);
+            return;
+        }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(used[i]) continue;
+            cur = cur + s[i];
+            used[i] = true;
+            dfs(s, cur, used);
+            used[i] = false;
+            cur.pop_back();
+        }
+    }
+
+    vector<string> permutation(string s) {
+        string cur = "";
+        vector<bool> used(s.size(), false);
+        dfs(s, cur, used);
+
+        for(auto& ele : store) res.push_back(ele);
+        return res;
+    }
+};
